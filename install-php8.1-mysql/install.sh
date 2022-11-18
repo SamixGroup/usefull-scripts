29cb18b9b1a213773def557a087025deb728f06e
Samandar <s.murodjonov@uzinfocom.uz>
2022-11-18 11:05:55 +0500

    added script for installation of php8.1-fpm and latest version of mysql

---
+++ install-php8.1-mysql/install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install ca-certificates apt-transport-https
+sudo add-apt-repository ppa:ondrej/php
+sudo apt install php8.1 php8.1-fpm
+sudo apt install php8.1-mysql
+sudo apt install mysql-server
+# sudo mysql_secure_installation
+sudo apt install composer
